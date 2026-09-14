57384312ec886c54862f3cc1a4922bb241140a6e
Aiden Schembri <aidsch0605@outlook.com>
2026-09-14 18:46:35 +0200

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Maqrut development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # Go backend
+              go
+              gopls
+              delve
+
+              # React frontend
+              nodejs_24
+              pnpm
+
+              # Database
+              sqlite
+            ];
+          };
+        }
+      );
+    };
+}
